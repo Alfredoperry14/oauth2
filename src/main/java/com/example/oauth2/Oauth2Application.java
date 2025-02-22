24e0b16fe9f36a128605ed6c0467b529d4be25c5
alfredoperry14 <alfredoperry14@gmail.com>
2025-02-22 13:11:49 -0800

    CSRF

--- src/main/java/com/example/oauth2/Oauth2Application.java
+++ src/main/java/com/example/oauth2/Oauth2Application.java
@@ -1,13 +1,58 @@
 package com.example.oauth2;
 
+import java.util.Map;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.Customizer;
 
+@EnableWebSecurity
 @SpringBootApplication
+@RestController
 public class Oauth2Application {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal){
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2Application.class, args);
 	}
 
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling((exceptions) -> exceptions
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+			.oauth2Login(Customizer.withDefaults())
+            // Configure CSRF to use a Cookie-based token repository.
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            )
+			.logout(logout -> logout
+				.logoutUrl("/logout")             // Endpoint to trigger logout
+				.logoutSuccessUrl("/")            // Where to redirect after logout
+				.deleteCookies("JSESSIONID")      // Clean up session cookies
+				.invalidateHttpSession(true)      // Invalidate the session
+			);			
+        return http.build();
+    }
 }
